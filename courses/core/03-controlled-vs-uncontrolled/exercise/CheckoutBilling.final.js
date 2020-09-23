218ec2f85d124c7d5f87403b2e73382545481193
inhyukchoi <inchoi.abc@gmail.com>
2020-09-22 17:40:15 -0700

    Update CheckoutBilling.final.js
    Hi Brad,
    
    I hope all is well with you. This is In from Disney. I took your course a while ago. I'm not proposing any changes to the code. I'm just going through exercises now that I have some time and I'm still a beginner when it comes to React. I ended up having a solution for this exercise that is a bit different from your final solution and just wanted to get your feedback.
    
    I basically created a custom hook for handling onChange because I like the idea of having one function for doing similar things. If you have time, let me know what you think of it and if I'm missing any thing important by doing it this way.
    
    Thanks!

--- courses/core/03-controlled-vs-uncontrolled/exercise/CheckoutBilling.final.js
+++ courses/core/03-controlled-vs-uncontrolled/exercise/CheckoutBilling.final.js
@@ -2,97 +2,114 @@ import React, { useState } from 'react'
 import { MdShoppingCart } from 'react-icons/md'
 import Heading from 'YesterTech/Heading'
 
+// A custom hook for handling controlled input
+const useInput = (initialValue) => {
+    const [value, setValue] = useState(initialValue)
+    return [
+        value,
+        e => setValue(e.target.value)
+    ]
+}
+
 function CheckoutBilling({ onSubmit }) {
-  const [sameAsBilling, setSameAsBilling] = useState(false)
-  const [billingName, setBillingName] = useState('')
-  const [billingAddress, setBillingAddress] = useState('')
-  const [shippingName, setShippingName] = useState('')
-  const [shippingAddress, setShippingAddress] = useState('')
+    const [sameAsBilling, setSameAsBilling] = useState(false)
+    const [billingName, setBillingName] = useInput('')
+    const [billingAddress, setBillingAddress] = useInput('')
+    const [shippingName, setShippingName] = useInput('')
+    const [shippingAddress, setShippingAddress] = useInput('')
 
-  function handleSubmit(event) {
-    event.preventDefault()
-    const fields = {
-      billingName,
-      billingAddress,
-      shippingName: sameAsBilling ? billingName : shippingName,
-      shippingAddress: sameAsBilling ? billingAddress : shippingAddress
+    function handleSubmit(event) {
+        event.preventDefault()
+        const fields = {
+            billingName,
+            billingAddress,
+            shippingName: sameAsBilling ? billingName : shippingName,
+            shippingAddress: sameAsBilling ? billingAddress : shippingAddress
+        }
+        onSubmit(sameAsBilling, fields)
     }
-    onSubmit(sameAsBilling, fields)
-  }
 
-  return (
-    <div className="spacing">
-      <Heading>
-        <MdShoppingCart /> Billing &amp; Shipping
+    function getShippingName() {
+        return sameAsBilling ? billingName : shippingName
+    }
+
+    function getShippingAddress() {
+        return sameAsBilling ? billingAddress : shippingAddress
+    }
+
+    return (
+        <div className="spacing">
+            <Heading>
+                <MdShoppingCart /> Billing &amp; Shipping
       </Heading>
-      <form onSubmit={handleSubmit} className="spacing">
-        <Heading as="h2" size={3}>
-          Billing Info
+            <form onSubmit={handleSubmit} className="spacing">
+                <Heading as="h2" size={3}>
+                    Billing Info
         </Heading>
-        <hr />
-        <div className="form-field">
-          <label htmlFor="billing:name">Name</label>
-          <input
-            id="billing:name"
-            type="text"
-            required
-            defaultValue={billingName}
-            onChange={event => setBillingName(event.target.value)}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="billing:address">Address</label>
-          <input
-            id="billing:address"
-            type="text"
-            required
-            defaultValue={billingAddress}
-            onChange={event => setBillingAddress(event.target.value)}
-          />
-        </div>
+                <hr />
+                <div className="form-field">
+                    <label htmlFor="billing:name">Name</label>
+                    <input
+                        id="billing:name"
+                        type="text"
+                        required
+                        defaultValue={billingName}
+                        onChange={setBillingName}
+                    />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="billing:address">Address</label>
+                    <input
+                        id="billing:address"
+                        type="text"
+                        required
+                        defaultValue={billingAddress}
+                        onChange={setBillingAddress}
+                    />
+                </div>
 
-        <Heading as="h2" size={3}>
-          Shipping Info
+                <Heading as="h2" size={3}>
+                    Shipping Info
         </Heading>
 
-        <label>
-          <input
-            type="checkbox"
-            defaultChecked={sameAsBilling}
-            onChange={() => setSameAsBilling(!sameAsBilling)}
-          />{' '}
+                <label>
+                    <input
+                        type="checkbox"
+                        defaultChecked={sameAsBilling}
+                        onChange={() => setSameAsBilling(!sameAsBilling)}
+                    />{' '}
           Same as Billing
         </label>
 
-        <div className="form-field">
-          <label htmlFor="shipping:name">Name</label>
-          <input
-            id="shipping:name"
-            type="text"
-            required
-            value={sameAsBilling ? billingName : shippingName}
-            onChange={event => setShippingName(event.target.value)}
-            disabled={sameAsBilling}
-          />
-        </div>
-        <div className="form-field">
-          <label htmlFor="shipping:address">Address</label>
-          <input
-            id="shipping:address"
-            type="text"
-            required
-            value={sameAsBilling ? billingAddress : shippingAddress}
-            onChange={event => setShippingAddress(event.target.value)}
-            disabled={sameAsBilling}
-          />
-        </div>
+                <div className="form-field">
+                    <label htmlFor="shipping:name">Name</label>
+                    <input
+                        id="shipping:name"
+                        type="text"
+                        required
+                        value={getShippingName()}
+                        onChange={setShippingName}
+                        disabled={sameAsBilling}
+                    />
+                </div>
+                <div className="form-field">
+                    <label htmlFor="shipping:address">Address</label>
+                    <input
+                        id="shipping:address"
+                        type="text"
+                        required
+                        value={getShippingAddress()}
+                        onChange={setShippingAddress}
+                        disabled={sameAsBilling}
+                    />
+                </div>
 
-        <button type="submit" className="button">
-          Submit
+                <button type="submit" className="button">
+                    Submit
         </button>
-      </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
 
 export default CheckoutBilling
